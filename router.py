5a4c1313cc31afecfa71784198e918f6ce49195a
Kaine <ivanovskija36@gmail.com>
2024-07-19 05:11:39 +0300

    Всё нахер переделано, обновился лендинг

--- router.py
+++ router.py
@@ -17,7 +17,7 @@ USER_URL = f'{API_BASE_URL}/users/@me'
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user=user)
 
 
 @app.route('/<filename>')
